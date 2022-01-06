44d2d7369cfb27b1ed55724682935892e395b274
Kevin <kevincespedes2004@gmail.com>
2022-01-05 23:56:50 -0500

    commit day 05

--- Js/filtro_busquedas.js
+++ Js/filtro_busquedas.js
@@ -3,7 +3,6 @@ export default function searchFilter(input, selector) {
   document.addEventListener("keyup", (e) => {
     if (e.target.matches(input)) {
       cards.forEach((item) => {
-        console.log(item.textContent.toLowerCase());
         item.textContent.toLowerCase().includes(e.target.value.toLowerCase())
           ? item.classList.remove("filter")
           : item.classList.add("filter");
